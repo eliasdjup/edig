fe745c7875b108c8753f2c07ffeb0668753ef317
Elias <eliasdjup@gmail.com>
2022-11-09 15:38:30 +0100

    fix

--- frontend/client.js
+++ frontend/client.js
@@ -4,5 +4,6 @@ import sanityClient from "@sanity/client";
 export default sanityClient({
   projectId: "h3zfxe8b", // you can find this in sanity.json
   dataset: "production", // or the name you chose in step 1
+  apiVersion: "2022-11-309",
   useCdn: true, // `false` if you want to ensure fresh data
 });
